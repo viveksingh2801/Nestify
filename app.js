e5cad8f63cdc05bfded5b2795ad3b122b6b4d278
Vivek Singh <vivek280197@gmail.com>
2024-11-19 12:04:33 +0530

    Add Project Files

--- app.js
+++ app.js
@@ -66,9 +66,7 @@ const sessionOptions = {
   },
 };
 
-// app.get("/", (req, res) => {
-//   res.send("Hi, I am root");
-// });
+
 
 app.use(session(sessionOptions));
 app.use(flash());
@@ -100,6 +98,10 @@ app.use((req, res, next) => {
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
+
+app.get("/", (req, res) => {
+  res.send("Hi, I am root");
+});
 
 app.all("*", (req, res, next) => {
   next(new ExpressError(404, "page  not found"));
